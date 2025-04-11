7aff509970edc017d176d69a21dbe0be98cc1fad
Huawei <eviningouaalphonsesorel@gamil.com>
2025-04-11 12:30:50 +0200

    second

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -11,7 +11,7 @@ import {NgForOf} from '@angular/common';
 })
 export class ProjectsComponent {
   projects = [
-    { name: 'Portfolio Angular', description: 'Un site personnel en Angular', link: '#' },
-    { name: 'Application de tâches', description: 'Un gestionnaire de tâches avec Firebase', link: '#' }
+    { name: 'Portfolio Angular', description: 'Un site personnel en Angular', link: 'https://portfolio-pj8kbym80-alphonse-sorels-projects.vercel.app/' },
+    { name: 'Applicattion de Liste de Pays', description: 'Un site qui affiche les pays du monde', link: 'https://tp-angular-api-tes-6xyjz7ey4-alphonse-sorels-projects.vercel.app/' }
   ];
 }
